fbc88702766f83214d9091b91199a30117284518
ksadej <66494934+ksadej@users.noreply.github.com>
2021-03-13 21:51:55 +0100

    Add files via upload

---
+++ Wzorcowy - Spring_test_14/src/test/java/com/Spring_test/_11/SpringTest14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_test._11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
